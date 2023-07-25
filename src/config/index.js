5831357ea371d36b01bf0c23d5e30a2957a561b0
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2023-07-25 16:45:03 +0530

    change server

--- src/config/index.js
+++ src/config/index.js
@@ -2,15 +2,13 @@ const hostname = window.location.hostname;
 
 // const SERVER_URL = "http://54.201.100.185:5000/api/v1";
 
-console.log(hostname);
-
 //
 const SERVER_URL =
   hostname == "localhost"
     ? "http://localhost:5000/api/v1"
-    : "http://35.93.141.181/api/v1";
+    : "http://35.92.116.43/api/v1";
 
 // Local Server
-// const SERVER_URL = "http://localhost:5000/api/v1";
+// const SERVER_URL = "http://35.92.116.43/api/v1";
 
 export { SERVER_URL };
